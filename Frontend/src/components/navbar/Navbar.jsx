b456f2819581edcec069a0880fafb9522675378d
Edison <aalevenegass@gmail.com>
2024-08-13 18:24:33 -0400

    Incorporando links para registro e inicio de sesión

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import "../navbar/navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="navbar__container">
-      <div>
+      <div className="px-[30px] py-[10px] flex items-center justify-between">
         <div>
-          <h2>URL Shortener</h2>
+          <h2 className="text-white font-medium">URL Shortener</h2>
+        </div>
+        <div className="flex items-center gap-3">
+          <Link className="text-white font-medium">Iniciar sesión</Link>
+          <Link className="text-white font-medium">Registrarse</Link>
         </div>
       </div>
     </nav>
