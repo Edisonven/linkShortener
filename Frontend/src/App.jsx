83fc6423242a484626c57b1adaacfea0db3a057e
Edison <aalevenegass@gmail.com>
2024-08-13 16:51:06 -0400

    IOnstalando y configurando tailwind

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <section className="app__container">
-      <h1>Holi </h1>
+      <h1 className="text-teal-600">Holi</h1>
     </section>
   );
 }
