096a573a45adc01e34d808747d3d12e5bcc0e5b3
Edison <aalevenegass@gmail.com>
2024-08-13 20:06:12 -0400

    Almacenando tema en localstorage

--- Frontend/src/components/darkModeSwitch/DarkModeSwitch.jsx
+++ Frontend/src/components/darkModeSwitch/DarkModeSwitch.jsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 
-export default function () {
-  const [theme, setTheme] = useState(false);
+export default function ThemeSwitcher() {
+  const [theme, setTheme] = useState("light");
 
   const handleChangeTheme = () => {
-    setTheme(!theme);
-    document.documentElement.classList.toggle("dark");
+    const newTheme = theme === "light" ? "dark" : "light";
+    document.documentElement.setAttribute("class", newTheme);
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
   };
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      document.documentElement.setAttribute("class", storedTheme);
+      setTheme(storedTheme);
+    }
+  }, []);
 
   return (
     <div>
-      {theme ? (
+      {theme === "light" ? (
         <MdLightMode
           onClick={handleChangeTheme}
           className="text-yellow-400 dark:text-slate-white cursor-pointer text-[25px] select-none"
