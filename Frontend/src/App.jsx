f1472acf6baf49e08e4ff1431374944e622f9118
Edison <aalevenegass@gmail.com>
2024-08-13 21:09:30 -0400

    Creando rutas para registro e inicio de sesión

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import Home from "../views/home/Home";
 import "./App.css";
+import Home from "../views/home/Home";
 import Navbar from "./components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "../views/signIn/SignIn";
+import SignUp from "../views/signUp/SignUp";
 
 function App() {
   return (
     <section className="app__container max-w-[1400px] mx-auto">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
     </section>
   );
 }
--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,23 +1,35 @@
 import "../navbar/navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BiLink } from "react-icons/bi";
 import DarkModeSwitch from "../darkModeSwitch/DarkModeSwitch";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleNavigateToHome = () => {
+    navigate("/");
+  };
+
   return (
     <nav className="navbar__container">
       <div className="px-[10px] py-[10px] sm:px-[30px] sm:py-[10px] flex items-center justify-between">
-        <div className="flex items-center gap-3">
+        <div onClick={handleNavigateToHome} className="flex items-center gap-3 cursor-pointer">
           <BiLink className="text-slate-800 text-xl dark:text-white" />
           <h2 className="text-slate-800 font-medium dark:text-white">
             URL Shortener
           </h2>
         </div>
         <div className="flex items-center gap-3">
-          <Link className="text-slate-800 font-medium text-sm sm:text-base dark:text-white">
+          <Link
+            to="sign-in"
+            className="text-slate-800 font-medium text-sm sm:text-base dark:text-white"
+          >
             Iniciar sesión
           </Link>
-          <Link className="text-slate-800 font-medium text-sm sm:text-base dark:text-white">
+          <Link
+            to="sign-up"
+            className="text-slate-800 font-medium text-sm sm:text-base dark:text-white"
+          >
             Registrarse
           </Link>
           <div>
