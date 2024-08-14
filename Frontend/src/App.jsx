26c35cd3550e831520d7c00600bdf2be050aa80a
Edison <aalevenegass@gmail.com>
2024-08-13 20:57:33 -0400

    Creando componente home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
+import Home from "../views/home/Home";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import UrlShortener from "./components/urlShortener/UrlShortener";
 
 function App() {
   return (
     <section className="app__container max-w-[1400px] mx-auto">
       <Navbar />
-      <UrlShortener />
+      <Home />
     </section>
   );
 }
---
+++ Frontend/views/home/Home.jsx
@@ -0,0 +1,9 @@
+import UrlShortener from "../../src/components/urlShortener/UrlShortener";
+
+export default function Home() {
+  return (
+    <section className="home__container">
+      <UrlShortener />
+    </section>
+  );
+}
