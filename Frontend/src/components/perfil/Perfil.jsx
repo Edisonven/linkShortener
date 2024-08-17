f5e368731252e920d311246d236f36ca925668da
Edison <aalevenegass@gmail.com>
2024-08-17 19:24:34 -0400

    Instalandod ependencia para animaciones de modal

--- Frontend/src/components/perfil/Perfil.jsx
+++ Frontend/src/components/perfil/Perfil.jsx
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUser } from "../../features/users/usersSlice";
 import { IoIosArrowDown } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { IoIosLogOut } from "react-icons/io";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Perfil() {
   const dispatch = useDispatch();
@@ -9,14 +12,22 @@ export default function Perfil() {
   const user = useSelector((state) => state.user);
   const { name, email, status, error } = user;
   const formatedName = name?.split(" ");
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     dispatch(fetchUser(userToken));
   }, [dispatch]);
 
+  const handleOpenModal = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
-    <section className="cursor-pointer">
-      <div className="flex items-center gap-1 text-white dark:text-slate-800 bg-slate-500 px-2 select-none rounded shadow dark:bg-slate-100">
+    <section className="cursor-pointer relative">
+      <div
+        onClick={handleOpenModal}
+        className="flex items-center gap-1 text-white dark:text-slate-800 bg-slate-500 px-2 select-none rounded shadow dark:bg-slate-100"
+      >
         {status === "loading" ? (
           ""
         ) : (
@@ -27,6 +38,24 @@ export default function Perfil() {
         )}
         <IoIosArrowDown />
       </div>
+      <AnimatePresence>
+        {openModal && (
+          <motion.div
+            className="modal bg-white absolute top-[50px] right-[0] w-[150px] h-[80px] rounded shadow flex items-center justify-center flex-col gap-2 p-5"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            transition={{ duration: 0.2 }}
+          >
+            <Link className="font-medium">Mi perfil</Link>
+            <hr className="w-full" />
+            <div className="flex items-center gap-1">
+              <Link className="text-sm font-medium">Cerrar sesión</Link>
+              <IoIosLogOut className="text-[20px]" />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 }
