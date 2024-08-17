19b55db6b19b0de07ab15b4e521611aa4065a5d0
Edison <aalevenegass@gmail.com>
2024-08-17 19:38:16 -0400

    Creando función para cerrar modal con clicks fuera de ella

--- Frontend/src/components/perfil/Perfil.jsx
+++ Frontend/src/components/perfil/Perfil.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUser } from "../../features/users/usersSlice";
 import { IoIosArrowDown } from "react-icons/io";
@@ -13,6 +13,8 @@ export default function Perfil() {
   const { name, email, status, error } = user;
   const formatedName = name?.split(" ");
   const [openModal, setOpenModal] = useState(false);
+  const modalRef = useRef(null);
+  const btnRef = useRef(null);
 
   useEffect(() => {
     dispatch(fetchUser(userToken));
@@ -22,9 +24,29 @@ export default function Perfil() {
     setOpenModal(!openModal);
   };
 
+  const handleClickOutside = (event) => {
+    if (
+      modalRef.current &&
+      btnRef.current &&
+      !modalRef.current.contains(event.target) &&
+      !btnRef.current.contains(event.target)
+    ) {
+      setOpenModal(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <section className="cursor-pointer relative">
       <div
+        ref={btnRef}
         onClick={handleOpenModal}
         className="flex items-center gap-1 text-white dark:text-slate-800 bg-slate-500 px-2 select-none rounded shadow dark:bg-slate-100"
       >
@@ -41,6 +63,7 @@ export default function Perfil() {
       <AnimatePresence>
         {openModal && (
           <motion.div
+            ref={modalRef}
             className="modal bg-white absolute top-[50px] right-[0] w-[150px] h-[80px] rounded shadow flex items-center justify-center flex-col gap-2 p-5"
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -49,7 +72,7 @@ export default function Perfil() {
           >
             <Link className="font-medium">Mi perfil</Link>
             <hr className="w-full" />
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1 mt-1">
               <Link className="text-sm font-medium">Cerrar sesión</Link>
               <IoIosLogOut className="text-[20px]" />
             </div>
