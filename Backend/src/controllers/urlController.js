f7362bbd8685731902d957c671ea8d9765968043
Edison <aalevenegass@gmail.com>
2024-08-20 11:18:34 -0400

    Quitando restricción de inicio de sesión para acortar links

--- Backend/src/controllers/urlController.js
+++ Backend/src/controllers/urlController.js
@@ -3,11 +3,19 @@ import { urlModels } from "../models/urlModels.js";
 
 const registerUrls = async (req, res) => {
   try {
+    let id = null;
     const authorization = req.header("Authorization");
-    const token = authorization.split("Bearer ")[1];
-    const { id } = jwt.decode(token);
+
+    if (authorization) {
+      const token = authorization.split("Bearer ")[1];
+      const decodedToken = jwt.decode(token);
+      if (decodedToken) {
+        id = decodedToken.id;
+      }
+    }
 
     const { longUrl } = req.body;
+
     if (!longUrl) {
       return res
         .status(404)
@@ -22,10 +30,21 @@ const registerUrls = async (req, res) => {
     const urls = await urlModels.createUrl(id, longUrl, shortUrl);
     res.status(201).json({ message: "Short-url created successfully", urls });
   } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+const getShortUrl = async (req, res) => {
+  try {
+    const { shortUrl } = req.body;
+  } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const urlController = {
   registerUrls,
+  getShortUrl,
 };
--- Backend/src/routes/urlRoutes.js
+++ Backend/src/routes/urlRoutes.js
@@ -3,6 +3,7 @@ import { urlController } from "../controllers/urlController.js";
 import verifyValidToken from "../middlewares/verifyToken.js";
 const router = Router();
 
-router.post("/", verifyValidToken, urlController.registerUrls);
+router.post("/", urlController.registerUrls);
+router.get("/short-url", verifyValidToken, urlController.getShortUrl);
 
 export default router;
