9ed8f3079ea5ecfa207b003f6fe524e9bbe75e74
Edison <aalevenegass@gmail.com>
2024-08-20 20:14:00 -0400

    Añadiendo feedback de enlace copiado al portapapeles

--- Frontend/src/components/urlShortener/UrlShortener.jsx
+++ Frontend/src/components/urlShortener/UrlShortener.jsx
@@ -8,7 +8,7 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import useFormSubmit from "../../../hooks/forms/useFormSubmit";
 import { regex } from "../../../utils/regex/regex";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import usePostUrl from "../../../hooks/users/usePostUrl";
 import useGetUrl from "../../../hooks/urls/useGetUrl";
 import config from "../../../config/config";
@@ -16,7 +16,8 @@ import { useParams } from "react-router-dom";
 import DefaultButton from "../buttons/DefaultButton";
 import { IoIosAlert } from "react-icons/io";
 import { MdOutlineFileCopy } from "react-icons/md";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function UrlShortener() {
   const { longUrl, errors } = useSelector((state) => state.urls);
@@ -26,6 +27,7 @@ export default function UrlShortener() {
   const dispatch = useDispatch();
   const { urlParams } = useParams();
   const urlRegex = regex.urlRegex;
+  const [isCopied, setIsCopied] = useState(false);
 
   const handleSendLongUrl = (e) => {
     e.preventDefault();
@@ -115,7 +117,7 @@ export default function UrlShortener() {
                 </div>
               )}
               {shortedUrl && (
-                <div className="flex items-center flex-col lg:flex-row justify-between w-full">
+                <div className="flex items-center flex-col lg:flex-row justify-between w-full relative">
                   <span className="text-slate-800 dark:text-white max-w-[300px] sm:max-w-full ml-4 text-ellipsis overflow-hidden whitespace-nowrap mt-2">
                     <span className="font-medium">Original:</span> {originalUrl}
                   </span>
@@ -123,19 +125,46 @@ export default function UrlShortener() {
                     whileTap={{ scale: 0.98 }}
                     className="w-[max-content] flex items-center gap-1 border-2 dark:border p-2 rounded-[30px] mt-3 sm:ml-auto sm:mr-5 cursor-pointer select-none dark:text-white hover:bg-gray-100 hover:dark:bg-slate-800 transition-colors duration-300"
                   >
-                    <span className=" text-[20px] sm:text-[23px] ">
-                      {config.frontendUrl}/
-                      <span className="text-red-600 font-normal">
-                        {shortedUrl}
-                      </span>
-                    </span>
+                    <CopyToClipboard
+                      text={`${config.frontendUrl}/${shortedUrl}`}
+                    >
+                      <div
+                        onClick={() => {
+                          setIsCopied(true);
+                          setTimeout(() => {
+                            setIsCopied(false);
+                          }, 2000);
+                        }}
+                        className="relative text-[20px] sm:text-[23px]"
+                      >
+                        {config.frontendUrl}/
+                        <span className="text-red-600 font-normal">
+                          {shortedUrl}
+                        </span>
+                        <AnimatePresence>
+                          {isCopied && (
+                            <motion.div
+                              initial={{ opacity: 0, scale: 0.9 }}
+                              animate={{ opacity: 1, scale: 1 }}
+                              exit={{ opacity: 0, scale: 0.9 }}
+                              transition={{ duration: 0.2 }}
+                              className="bg-white shadow rounded h-[50px] absolute top-[50px] right-[20px] p-3 flex items-center justify-center dark:bg-[#161B22] dark:text-white pointer-events-none"
+                            >
+                              <span className="text-slate-800 dark:text-white text-sm font-medium">
+                                texto copiado al portapapeles
+                              </span>
+                            </motion.div>
+                          )}
+                        </AnimatePresence>
+                      </div>
+                    </CopyToClipboard>
                     <MdOutlineFileCopy className="text-[20px]" />
                   </motion.div>
                 </div>
               )}
             </div>
             <DefaultButton
-              className="absolute top-[5px] right-[8px] default-button text-white font-medium dark:text-slate-800 flex items-center gap-1 bg-teal-700 dark:bg-gray-300"
+              className="absolute top-[5px] right-[8px] default-button text-white font-medium dark:text-slate-800 flex items-center gap-1 bg-teal-700 dark:bg-gray-300 select-none"
               type="submit"
             >
               Acortar URL
