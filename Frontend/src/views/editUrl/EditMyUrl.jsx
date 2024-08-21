c2085bdbc220155aeba9be7f7952d33317271a43
Edison <aalevenegass@gmail.com>
2024-08-20 23:05:28 -0400

    Ceeando formulario para envío de datos actualizados

--- Frontend/src/views/editUrl/EditMyUrl.jsx
+++ Frontend/src/views/editUrl/EditMyUrl.jsx
@@ -1,7 +1,61 @@
+import DefaultButton from "../../components/buttons/DefaultButton";
+
 export default function EditMyUrl() {
+  const handleSendUpdatedInfo = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <section>
-      <h1> hola soy el editar</h1>
+    <section className="mt-[10px] sm:mt-[30px] max-w-[600px] mx-auto p-4">
+      <h1 className="text-slate-800 dark:text-white font-medium text-[30px] mb-2">
+        Editar URL
+      </h1>
+      <div className="bg-white dark:bg-[#161B22] shadow rounded p-4">
+        <form className="flex flex-col gap-5">
+          <div>
+            <label
+              className="text-slate-800 dark:text-white font-medium text-[15px]"
+              htmlFor="long-url"
+            >
+              Enlace largo
+            </label>
+            <input
+              className="base-input bg-white dark:bg-[#161B22] text-slate-800 dark:text-white mt-1"
+              type="text"
+            />
+          </div>
+          <div>
+            <label
+              className="text-slate-800 dark:text-white font-medium text-[15px]"
+              htmlFor="long-url"
+            >
+              Título
+            </label>
+            <input
+              className="base-input bg-white dark:bg-[#161B22] text-slate-800 dark:text-white mt-1"
+              type="text"
+            />
+          </div>
+          <div>
+            <label
+              className="text-slate-800 dark:text-white font-medium text-[15px]"
+              htmlFor="long-url"
+            >
+              Enlace corto
+            </label>
+            <input
+              className="base-input bg-white dark:bg-[#161B22] text-slate-800 dark:text-white mt-1 pointer-events-none"
+              type="text"
+            />
+          </div>
+          <DefaultButton
+            onClick={handleSendUpdatedInfo}
+            className="default-button w-[max-content] text-white font-medium dark:text-slate-800 flex items-center gap-1 bg-teal-700 dark:bg-gray-300 select-none"
+          >
+            Actualizar
+          </DefaultButton>
+        </form>
+      </div>
     </section>
   );
 }
