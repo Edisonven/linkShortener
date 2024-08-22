aee4c4f09a86c32448545e58be36093475cd2fc3
Edison <aalevenegass@gmail.com>
2024-08-22 12:42:59 -0400

    Agregando conección local

--- Backend/src/database/connection.js
+++ Backend/src/database/connection.js
@@ -4,6 +4,11 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   //  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   allowExitOnIdle: true,
 });
 
