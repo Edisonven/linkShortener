b83c38e180e3e326a15b67d37a03c56956e2f9f7
Edison <aalevenegass@gmail.com>
2024-08-22 23:18:01 -0400

    Agregando logo a la aplicación

--- Backend/src/database/connection.js
+++ Backend/src/database/connection.js
@@ -2,14 +2,13 @@ import pkg from "pg";
 import "dotenv/config";
 const { Pool } = pkg;
 
-/* user: process.env.DB_USER,
-host: process.env.DB_HOST,
-database: process.env.DB_DB,
-password: process.env.DB_PASSWORD,
-port: process.env.DB_PORT, */
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  //  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   allowExitOnIdle: true,
 });
 
--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import DarkModeSwitch from "../darkModeSwitch/DarkModeSwitch";
 import { useSelector } from "react-redux";
 import Invited from "../invited/Invited";
 import Perfil from "../perfil/Perfil";
+import logo from "/images/application/logo.png";
 
 export default function Navbar() {
   const userToken = useSelector((state) => state.userToken.token);
@@ -17,14 +18,12 @@ export default function Navbar() {
   return (
     <nav className="navbar__container bg-white dark:bg-[#161B22] shadow-sm dark:shadow-gray-800 h-[60px]">
       <div className="h-full px-[10px] py-[10px] sm:px-[30px] sm:py-[10px] flex items-center justify-between max-w-[1600px] mx-auto">
-        <div
-          onClick={handleNavigateToHome}
-          className="flex items-center gap-3 cursor-pointer"
-        >
-          <BiLink className="text-slate-800 text-xl dark:text-white" />
-          <h2 className="text-slate-800 font-medium dark:text-white">
-            URL Shortener
-          </h2>
+        <div className="flex items-center gap-2">
+          <p className="text-slate-800 dark:text-white font-semibold">Short</p>
+          <figure className="cursor-pointer" onClick={handleNavigateToHome}>
+            <img className="w-full max-w-[50px]" src={logo} alt="" />
+          </figure>
+          <p className="text-slate-800 dark:text-white font-semibold">URL</p>
         </div>
         <div className="flex items-center gap-2">
           {userToken ? <Perfil /> : <Invited />}
