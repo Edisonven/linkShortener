e536c5a92d7cf0081a107f4be3e617ad71b0922a
Edison <aalevenegass@gmail.com>
2024-08-22 20:48:34 -0400

    Añadiendo info para conocimiento de eliminación de cuenta

--- Frontend/src/views/MyProfile/DeleteUserAccount.jsx
+++ Frontend/src/views/MyProfile/DeleteUserAccount.jsx
@@ -1,7 +1,36 @@
+import DefaultButton from "../../components/buttons/DefaultButton";
+
 export default function DeleteUserAccount() {
   return (
-    <section>
-      <h1>Hola</h1>
+    <section className="mt-[10px] sm:mt-[30px] max-w-[1000px] mx-auto p-4">
+      <div className="max-w-[600px] mx-auto p-6 shadow rounded bg-white dark:bg-[#161B22]">
+        <h1 className="text-slate-800 dark:text-white font-normal text-xl text-center">
+          Eliminación de mi cuenta
+        </h1>
+        <p className="text-center text-slate-800 dark:text-white mt-4 font-normal">
+          Lamentamos esta noticia... si continua , esta cuenta no podrá
+          recuperarse.
+        </p>
+        <div className="flex items-baseline gap-2 mt-4">
+          <input type="checkbox" />
+          <p className="text-slate-800 dark:text-white font-medium">
+            Entiendo que al eliminar mi cuenta, se eliminará toso los enlaces
+            cortos asociados a esta cuenta, asumiendo lo que esto implique en
+            mis datos almacenados.{" "}
+            <span className="font-medium text-red-600">
+              Esta acción es irreversible*.
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-3 justify-center mt-6">
+          <DefaultButton className="default-button w-[max-content] text-white font-medium dark:text-slate-800 flex items-center gap-1 bg-teal-700 dark:bg-gray-300 select-none px-[15px] py-[10px] rounded-[30px] relative overflow-hidden">
+            Eliminar
+          </DefaultButton>
+          <button className="font-medium hover:text-teal-500 text-slate-800 dark:text-white hover:dark:text-teal-400">
+            Cancelar
+          </button>
+        </div>
+      </div>
     </section>
   );
 }
